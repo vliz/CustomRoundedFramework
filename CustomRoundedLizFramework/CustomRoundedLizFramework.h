3fde0237e6a5fd559a03923d3da1043345b81c6d
vliz <nicoleagnesfelicia@gmail.com>
2020-10-20 15:13:44 +0700

    Initial Commit

---
+++ CustomRoundedLizFramework/CustomRoundedLizFramework.h
@@ -0,0 +1,18 @@
+//
+//  CustomRoundedLizFramework.h
+//  CustomRoundedLizFramework
+//
+//  Created by Agnes Felicia on 20/10/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomRoundedLizFramework.
+FOUNDATION_EXPORT double CustomRoundedLizFrameworkVersionNumber;
+
+//! Project version string for CustomRoundedLizFramework.
+FOUNDATION_EXPORT const unsigned char CustomRoundedLizFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomRoundedLizFramework/PublicHeader.h>
+
+
